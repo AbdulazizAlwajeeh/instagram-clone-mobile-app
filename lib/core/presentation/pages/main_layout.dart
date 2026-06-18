efc963420ee4be7f7c6395feade9d586f00cdfca
AbdulazizAlwajeeh <3z0zalwajeeh@gmail.com>
2026-06-18 10:22:51 +0300

    Fixing the back button to go to feed page instead of exiting the app.

--- lib/core/presentation/pages/main_layout.dart
+++ lib/core/presentation/pages/main_layout.dart
@@ -15,24 +15,36 @@ class MainLayoutPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: navigationShell,
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: navigationShell.currentIndex,
-        onTap: _onTap,
-        type: BottomNavigationBarType.fixed,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Feed'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_box_outlined),
-            label: 'Post',
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.send), label: 'Chat'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
+    final bool isNotAtHomeTab = navigationShell.currentIndex != 0;
+
+    return PopScope(
+      canPop: !isNotAtHomeTab,
+      onPopInvokedWithResult: (didPop, result) {
+        if (didPop) return;
+
+        if (isNotAtHomeTab) {
+          navigationShell.goBranch(0, initialLocation: false);
+        }
+      },
+      child: Scaffold(
+        body: navigationShell,
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: navigationShell.currentIndex,
+          onTap: _onTap,
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Feed'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_box_outlined),
+              label: 'Post',
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.send), label: 'Chat'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+        ),
       ),
     );
   }
